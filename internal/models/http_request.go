bc36f51452622520f0686c1bd280f59aa4d40cbd
Whyayen <whyayen@gmail.com>
2024-07-02 16:37:18 +0800

    feat: change parameters type of HttpRequest

--- internal/models/http_request.go
+++ internal/models/http_request.go
@@ -1,18 +1,22 @@
 package models
 
+import "strings"
+
 type HttpRequest struct {
-	Host       string
-	Path       string
-	Method     string
-	Scheme     string
-	Headers    map[string]string
-	Parameters map[string]string
+	Host           string
+	Path           string
+	Method         string
+	Scheme         string
+	Headers        map[string]string
+	Parameters     string
+	InvalidRequest bool // flag for invalid request when parsing
 }
 
 func NewHttpRequest() *HttpRequest {
 	return &HttpRequest{
-		Headers:    make(map[string]string),
-		Parameters: make(map[string]string),
+		Headers:        make(map[string]string),
+		Parameters:     "",
+		InvalidRequest: false,
 	}
 }
 
@@ -36,6 +40,14 @@ func (r *HttpRequest) GetHeaders() map[string]string {
 	return r.Headers
 }
 
-func (r *HttpRequest) GetParameters() map[string]string {
+func (r *HttpRequest) GetParameters() string {
 	return r.Parameters
+}
+
+func (r *HttpRequest) IsValid() bool {
+	return r.Host != "" && r.Method != "" && r.Scheme != "" && !r.InvalidRequest
+}
+
+func (r *HttpRequest) IsGet() bool {
+	return strings.ToUpper(r.Method) == "GET"
 }
--- internal/models/http_request_test.go
+++ internal/models/http_request_test.go
@@ -6,53 +6,98 @@ import (
 )
 
 func TestGetHost(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Host = "example.com"
+	request := NewHttpRequest()
+	request.Host = "example.com"
 
-	assert.Equal(t, restful.GetHost(), "example.com")
+	assert.Equal(t, request.GetHost(), "example.com")
 }
 
 func TestGetPath(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Path = "/users"
+	request := NewHttpRequest()
+	request.Path = "/users"
 
-	assert.Equal(t, restful.GetPath(), "/users")
+	assert.Equal(t, request.GetPath(), "/users")
 }
 
 func TestGetMethod(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Method = "GET"
+	request := NewHttpRequest()
+	request.Method = "GET"
 
-	assert.Equal(t, restful.GetMethod(), "GET")
+	assert.Equal(t, request.GetMethod(), "GET")
 }
 
 func TestGetScheme(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Scheme = "https"
+	request := NewHttpRequest()
+	request.Scheme = "https"
 
-	assert.Equal(t, restful.GetScheme(), "https")
+	assert.Equal(t, request.GetScheme(), "https")
 }
 
 func TestGetHeaders(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Headers = map[string]string{
+	request := NewHttpRequest()
+	request.Headers = map[string]string{
 		"Authorization": "Bearer token",
 	}
 
-	assert.Equal(t, restful.GetHeaders(), map[string]string{
+	assert.Equal(t, request.GetHeaders(), map[string]string{
 		"Authorization": "Bearer token",
 	})
 }
 
 func TestGetParameters(t *testing.T) {
-	restful := NewHttpRequest()
-	restful.Parameters = map[string]string{
-		"id":         "66838a1d337a8cdc830b439c",
-		"subscribed": "true",
-	}
+	request := NewHttpRequest()
+	request.Parameters = "{\"id\":\"66838a1d337a8cdc830b439c\",\"subscribed\":\"true\"}"
 
-	assert.Equal(t, restful.GetParameters(), map[string]string{
-		"id":         "66838a1d337a8cdc830b439c",
-		"subscribed": "true",
-	})
+	assert.Equal(t, request.GetParameters(), "{\"id\":\"66838a1d337a8cdc830b439c\",\"subscribed\":\"true\"}")
+}
+
+func TestIsValid_WhenHostIsNil(t *testing.T) {
+	request := NewHttpRequest()
+	request.Method = "GET"
+	request.Scheme = "https"
+
+	assert.Equal(t, request.IsValid(), false)
+}
+
+func TestIsValid_WhenHostIsEmpty(t *testing.T) {
+	request := NewHttpRequest()
+	request.Host = ""
+	request.Method = "GET"
+	request.Scheme = "https"
+
+	assert.Equal(t, request.IsValid(), false)
+}
+
+func TestIsValid_WhenInvalidRequestIsTrue(t *testing.T) {
+	request := NewHttpRequest()
+	request.Host = "example.com"
+	request.Method = "GET"
+	request.Scheme = "https"
+	request.InvalidRequest = true
+
+	assert.Equal(t, request.IsValid(), false)
+
+}
+
+func TestIsValid_WhenHostAndMethodAndSchemeAreNotNil(t *testing.T) {
+	request := NewHttpRequest()
+	request.Host = "example.com"
+	request.Method = "GET"
+	request.Scheme = "https"
+
+	assert.Equal(t, request.IsValid(), true)
+}
+
+func TestIsGet_WhenMethodIsGet(t *testing.T) {
+	request := NewHttpRequest()
+	request.Method = "GET"
+
+	assert.Equal(t, request.IsGet(), true)
+}
+
+func TestIsGet_WhenMethodIsNotGet(t *testing.T) {
+	request := NewHttpRequest()
+	request.Method = "POST"
+
+	assert.Equal(t, request.IsGet(), false)
 }
