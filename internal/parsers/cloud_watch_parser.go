274132a15d87da7ff09a481ce31525a6f2c027f6
Whyayen <whyayen@gmail.com>
2024-07-02 13:36:50 +0800

    feat: Add CloudWatchParser

---
+++ internal/parsers/cloud_watch_parser.go
@@ -0,0 +1,43 @@
+package parsers
+
+import (
+	"github.com/whyayen/log2curl/internal/models"
+	"slices"
+	"strings"
+)
+
+type CloudWatchParser struct {
+	Config *models.RestfulConfiguration
+}
+
+func NewGeneralParser(config *models.RestfulConfiguration) *CloudWatchParser {
+	return &CloudWatchParser{
+		Config: config,
+	}
+}
+
+func (p *CloudWatchParser) Parse(log *map[string]string) *models.Restful {
+	restful := models.NewRestful(p.Config)
+
+	for key, value := range *log {
+		switch {
+		case key == p.Config.Host:
+			restful.Host = value
+		case key == p.Config.Path:
+			restful.Path = value
+		case key == p.Config.Method:
+			restful.Method = value
+		case key == p.Config.Scheme:
+			restful.Scheme = value
+		case strings.HasPrefix(key, p.Config.HeaderPrefix+"."):
+			header := strings.TrimPrefix(key, p.Config.HeaderPrefix+".")
+			if slices.Contains(p.Config.WhitelistHeaders, header) {
+				restful.Headers[header] = value
+			}
+		case strings.HasPrefix(key, p.Config.ParameterPrefix+"."):
+			restful.Parameters[strings.TrimPrefix(key, p.Config.ParameterPrefix+".")] = value
+		}
+	}
+
+	return restful
+}
---
+++ internal/parsers/cloud_watch_parser_test.go
@@ -0,0 +1,45 @@
+package parsers
+
+import (
+	"github.com/magiconair/properties/assert"
+	"github.com/whyayen/log2curl/internal/models"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	cfg := &models.RestfulConfiguration{
+		Host:             "host",
+		Path:             "path",
+		Method:           "method",
+		Scheme:           "scheme",
+		HeaderPrefix:     "header",
+		ParameterPrefix:  "parameter",
+		WhitelistHeaders: []string{"Authorization"},
+		CustomHost:       "custom.example2.com",
+	}
+	parser := NewGeneralParser(cfg)
+	log := map[string]string{
+		"host":                 "example.com",
+		"header.Authorization": "Bearer token",
+		"header.Content-Type":  "application/json",
+		"header.User-Agent":    "Webkit 1.0",
+		"parameter.id":         "66838a1d337a8cdc830b439c",
+		"parameter.subscribed": "true",
+		"path":                 "/users",
+		"method":               "GET",
+		"scheme":               "https",
+	}
+	result := parser.Parse(&log)
+
+	assert.Equal(t, result.GetHost(), "custom.example2.com")
+	assert.Equal(t, result.GetPath(), "/users")
+	assert.Equal(t, result.GetMethod(), "GET")
+	assert.Equal(t, result.GetScheme(), "https")
+	assert.Equal(t, result.GetHeaders(), map[string]string{
+		"Authorization": "Bearer token",
+	})
+	assert.Equal(t, result.GetParameters(), map[string]string{
+		"id":         "66838a1d337a8cdc830b439c",
+		"subscribed": "true",
+	})
+}
